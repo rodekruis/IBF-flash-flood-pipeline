39585e3d828451678711361fd76cc6febdb2f53d
JanvE97 <112380765+JanvE97@users.noreply.github.com>
2024-12-11 11:30:57 +0100

    update to prod vars

--- flash_flood_pipeline/settings/credentials.py
+++ flash_flood_pipeline/settings/credentials.py
@@ -1,20 +1,10 @@
-# import os
-
-# # api
-# API_SERVICE_URL = os.environ["IBF_URL"]
-# API_USERNAME = os.environ["ADMIN_LOGIN"]
-# API_ADMIN_PASSWORD = os.environ["IBF_PASSWORD"]
-
-# # malawi sensor satellite data
-# SENSOR_USERNAME = os.environ["SENSOR_USERNAME"]
-# SENSOR_PASSWORD = os.environ["SENSOR_PASSWORD"]
+import os
 
 # api
-API_SERVICE_URL = r"https://ibf-test.510.global/api/"  # r"https://ibf.510.global/api/"  # os.environ.get("IBF_URL")
-#API_SERVICE_URL = r"https://8b0a-2a02-a457-2a5d-1-18ad-c62b-f02-65c8.ngrok-free.app/api/"
-API_USERNAME = "dunant@redcross.nl"  # "rhdhv-mwi-flash-floods-pipeline@redcross.nl"  # os.environ.get("ADMIN_LOGIN")
-API_ADMIN_PASSWORD = "password"  # "54dkFVp83"  # os.environ.get("IBF_PASSWORD")
+API_SERVICE_URL = os.environ["IBF_URL"]
+API_USERNAME = os.environ["ADMIN_LOGIN"]
+API_ADMIN_PASSWORD = os.environ["IBF_PASSWORD"]
 
 # malawi sensor satellite data
-SENSOR_USERNAME = "malawisensordata@gmail.com"
-SENSOR_PASSWORD = "yoyg puuz zgkj zsuy"
+SENSOR_USERNAME = os.environ["SENSOR_USERNAME"]
+SENSOR_PASSWORD = os.environ["SENSOR_PASSWORD"]
