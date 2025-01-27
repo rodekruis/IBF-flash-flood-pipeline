a57389342a0be32050ee2e124035c309f817f25f
JanvE97 <112380765+JanvE97@users.noreply.github.com>
2025-01-27 15:18:25 +0100

    Update credentials.py

--- flash_flood_pipeline/settings/credentials.py
+++ flash_flood_pipeline/settings/credentials.py
@@ -1,10 +1,10 @@
 import os
 
 # api
-API_SERVICE_URL = r"https://ibf-test.510.global/api/" # os.environ["IBF_URL"]
-API_USERNAME = "dunant@redcross.nl" # os.environ["ADMIN_LOGIN"]
-API_ADMIN_PASSWORD = "password" #os.environ["IBF_PASSWORD"]
+API_SERVICE_URL = os.environ["IBF_URL"]
+API_USERNAME = os.environ["ADMIN_LOGIN"]
+API_ADMIN_PASSWORD = os.environ["IBF_PASSWORD"]
 
 # malawi sensor satellite data
-SENSOR_USERNAME = "malawisensordata@gmail.com" # os.environ["SENSOR_USERNAME"]
-SENSOR_PASSWORD = "yoyg puuz zgkj zsuy" # os.environ["SENSOR_PASSWORD"]
+SENSOR_USERNAME = os.environ["SENSOR_USERNAME"]
+SENSOR_PASSWORD = os.environ["SENSOR_PASSWORD"]
